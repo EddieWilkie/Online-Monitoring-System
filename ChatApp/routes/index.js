1d41af04e7862322b66cfae3b937b3a6ae6f08bc
Edward Wilkie <Edward Wilkie>
2016-12-19 01:51:05 +0000

    Updated UI added in extra features such as number of users online. Still need to refactor code before adding in the chat logger.

--- ChatApp/routes/index.js
+++ ChatApp/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'SocketIO' });
+  res.render('index', { title: 'SocketIO | Simple Chat Application' });
 });
 
 module.exports = router;
