fbb0666c3ef3113fbed84e20e3bbdb137c9bc138
Edward Wilkie <Edward Wilkie>
2016-12-20 23:11:03 +0000

    Chat logger logs files to log.txt

---
+++ ChatApp/logger.js
@@ -0,0 +1,18 @@
+/**
+ * Created by eddie on 20/12/2016.
+ */
+var fs = require('fs');
+
+function logging(data){
+    fs.appendFile('./log.txt',data,function(err){
+        if(err){
+            console.error(err);
+        }
+        console.log('Message Appended');
+    });
+}
+
+function logger() {
+    this.log = logging;
+}
+module.exports = new logger();
